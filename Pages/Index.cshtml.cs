c37a5e8196039ef316c1ea4e96e764dfd25404fa
sonishilpa1906 <sonishilpa1906@gmail.com>
2026-06-17 19:17:09 +0530

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace demo_web_app.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
